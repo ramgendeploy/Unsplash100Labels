1020fc48f520d6653a59963dc8c425b93450c9d2
Rama <rg1998@hotmail.es>
2019-07-26 17:09:48 -0300

    send data

--- app/server.py
+++ app/server.py
@@ -64,8 +64,8 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    prediction = learn.predict(img)
+    return JSONResponse({'result': prediction})
 
 
 if __name__ == '__main__':
