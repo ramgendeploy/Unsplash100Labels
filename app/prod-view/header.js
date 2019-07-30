7b6cfae51e3d21f191e4383c0529c16d3e8a3798
Rama <rg1998@hotmail.es>
2019-07-30 19:53:16 -0300

    header update

--- app/prod-view/header.js
+++ app/prod-view/header.js
@@ -2,7 +2,7 @@ function Header() {
     return React.createElement(
         'nav',
         { className: 'title' },
-        'Unsplash 100(ish\uD83D\uDE05) label classifier'
+        'Unsplash 100 label classifier'
     );
 }
 
